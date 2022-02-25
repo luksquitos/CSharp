f0c0bf70fdeda8dcd2202ad1c04b196e42db1247
Lucas <lucasmartinscaet@hotmail.com>
2022-02-25 18:10:22 -0300

    um exercicio

---
+++ Armazenando nome e somando números/Armazenando nome e somando números/Program.cs
@@ -0,0 +1,29 @@
+﻿// Mostrando como uma simples coisa em Python em outra linguagem é quase impossível de ser lida.
+
+using System;
+
+namespace Armazenando_nome_e_somando_números
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            int n1;
+            int n2;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine(); // Essa linha pode ser escrita dessa forma porque é string.
+            Console.WriteLine($"Okay, {nome}... Digite 2 números para serem somados: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            var soma = n1 + n2;
+
+            Console.WriteLine($"A soma de {n1} + {n2} é {soma}");
+
+
+        }
+    }
+}
