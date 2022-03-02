6afc97bf28cd2711df84bb24645be798d73ca8c8
Lucas <lucasmartinscaet@hotmail.com>
2022-03-02 11:41:26 -0300

    mais dois exercicios

---
+++ Adicionando nomes em Arrays/Adicionando nomes em Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Adicionando_nomes_em_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite quantos nomes deseja cadastrar: ");
+            int num = Convert.ToInt16(Console.ReadLine());
+            string[] nomes = new string[num];
+
+            //Cadastrando os nomes
+            int totalCadastrados = 0;
+            for (int index = 0; index < nomes.Length; index++)
+            {
+                Console.Write($"{index + 1}º nome: ");
+                nomes[index] = Console.ReadLine();
+                totalCadastrados += 1;
+            }
+
+            //Mostrando os nomes
+            Console.WriteLine($"Os {totalCadastrados} nomes cadastrados foram...");
+            for (int index = 0; index < nomes.Length; index++)
+            {
+                Console.WriteLine($"{nomes[index]}");
+            }
+            
+            
+        }
+    }
+}
---
+++ Números primos/Números primos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Números_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esse programa irá encontrar todos os números primos até o que você escrever");
+            Console.Write("Digite um número :");
+
+            int max = Convert.ToInt16(Console.ReadLine());
+            
+
+            for (int a = 1; a <= max; a++)
+            {
+                int contDivi = 0;
+                for (int b = 1; b <= a; b++)
+                {
+                    if (a % b == 0)
+                    {
+                        contDivi++;
+                    }
+                }
+                if (contDivi <= 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write($"{a} ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{a} ");
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
